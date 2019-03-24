aeac523f3396bd357a3ab5611a98215b0e124e04
Kernael92 <kernaelapuko5@gmail.com>
2019-03-24 23:46:43 +0300

    create the blog bueprint and the index template

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,8 +28,14 @@ def create_app(test_config=None):
     def hello():
         return 'Hello World!'
 
-    from . import db, auth
+    from . import db
     db.init_app(app)
-    app.register_blueprint(auth.app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -4,11 +4,11 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-from flaskr.db import get-db
+from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', method=('GET', 'POST'))
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -16,7 +16,7 @@ def register():
         db = get_db()
         error = None
 
-        if no username:
+        if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
@@ -83,5 +83,5 @@ def login_required(view):
         if g.user is None:
             return redirect(url_for('auth,login'))
         return view(**kwargs)
-        
+
     return wrapped_view
